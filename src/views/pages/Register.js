e5626a328cb44ce899e3127bdccce22f4a5220d0
chetanRawat <chetanrawat560933@gmail.com>
2023-06-19 22:04:11 +0530

    Registration functionality implemented. On clicking register- getting redirected to Dashboard

--- src/views/pages/Register.js
+++ src/views/pages/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -13,8 +13,21 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked } from '@coreui/icons'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const history = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (email && password && confirmPassword) {
+      history('/dashboard')
+    }
+  }
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -22,12 +35,18 @@ const Register = () => {
           <CCol md={9} lg={7} xl={6}>
             <CCard className="mx-4">
               <CCardBody className="p-4">
-                <CForm>
+                <CForm onSubmit={handleSubmit}>
                   <h1>Register</h1>
                   <p className="text-medium-emphasis">Create your account</p>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    <CFormInput
+                      type="email"
+                      placeholder="Email"
+                      autoComplete="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -37,20 +56,37 @@ const Register = () => {
                       type="password"
                       placeholder="Password"
                       autoComplete="new-password"
+                      minLength={6}
+                      maxLength={12}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
                     />
                   </CInputGroup>
-                  <CInputGroup className="mb-4">
+                  <CInputGroup className="mb-2">
                     <CInputGroupText>
                       <CIcon icon={cilLockLocked} />
                     </CInputGroupText>
                     <CFormInput
+                      style={{ padding: '10px' }}
                       type="password"
                       placeholder="Confirm password"
                       autoComplete="new-password"
+                      minLength={6}
+                      maxLength={12}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      required
                     />
                   </CInputGroup>
+                  {password === confirmPassword ? null : password !== '' &&
+                    confirmPassword !== '' ? (
+                    <div className="mb-2">
+                      <span style={{ color: 'red' }}>Confirm password must match password.</span>
+                    </div>
+                  ) : null}
                   <div className="d-grid">
-                    <CButton color="primary">Create Account</CButton>
+                    <CButton type="submit" color="primary">
+                      Create Account
+                    </CButton>
                   </div>
                 </CForm>
               </CCardBody>
