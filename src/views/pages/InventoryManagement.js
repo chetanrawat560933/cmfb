404f1002ffc50572eb4e4f777cca0a8803dd3986
saloniSoni <saloni2412@gmail.com>
2023-07-09 00:45:07 +0530

    Updated UI of registeration, login, dashboard. Table creation and api integration for Users, food banks.

--- src/views/pages/InventoryManagement.js
+++ src/views/pages/InventoryManagement.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const InventoryManagement = () => {
-  return <div>Work in progress</div>
+  const [data, getData] = useState([])
+  const URL = 'https://jsonplaceholder.typicode.com/posts'
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response)
+        getData(response)
+      })
+  }
+
+  return (
+    <table className="table table-hover table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default InventoryManagement
