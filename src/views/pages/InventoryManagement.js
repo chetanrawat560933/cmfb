0e33b353007cc664c417edb60bea727a49b16468
chetanrawat560933 <chetanrawat560933@gmail.com>
2023-07-22 03:45:18 +0530

    inventory management api integration

--- src/views/pages/InventoryManagement.js
+++ src/views/pages/InventoryManagement.js
@@ -1,7 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import json from '../../db/db.json'
+import axios from "axios";
 
 const InventoryManagement = () => {
+  const [inventoryData, setInventoryData] = useState([]);
+  const URL = "http://localhost:5040/cmfb/inventory/getAllInventory";
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      axios
+        .get(URL)
+        .then((response) => {
+          return response.data;
+        })
+        .then((data) => {
+          console.log(data);
+          setInventoryData(data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.error);
+        });
+    } catch (error) {
+      console.log(error.response);
+      return error.response;
+    }
+  };
+
   return (
     <table className="table table-hover table-striped">
       <thead>
@@ -13,14 +40,16 @@ const InventoryManagement = () => {
         </tr>
       </thead>
       <tbody>
-        {json.inventory.map((item, i) => (
-          <tr key={i}>
-            <td>{item.item_id}</td>
-            <td>{item.item_name}</td>
-            <td>{item.price}</td>
-            <td>{item.description}</td>
-          </tr>
-        ))}
+        {inventoryData.length !== 0 ?
+          inventoryData.map((item, i) => (
+            <tr key={i}>
+              <td>{item.item_id}</td>
+              <td>{item.item_name}</td>
+              <td>{item.price}</td>
+              <td>{item.description}</td>
+            </tr>
+          )) : <p>No records found.</p>
+        }
       </tbody>
     </table>
   )
