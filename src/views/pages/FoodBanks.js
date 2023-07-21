88c73a26962adae6dc79842e8f96954a099d05f0
chetanrawat560933 <chetanrawat560933@gmail.com>
2023-07-22 03:09:09 +0530

    API integration in foodbanks

--- src/views/pages/FoodBanks.js
+++ src/views/pages/FoodBanks.js
@@ -1,24 +1,53 @@
 import React, { useEffect, useState } from 'react'
-import json from '../../db/db.json'
+import axios from "axios";
 
 const FoodBanks = () => {
+  const [foodbanks, setFoodbanks] = useState([]);
+  const URL = "http://localhost:5040/cmfb/foodBank/getAllFoodBanks";
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      axios
+        .get(URL)
+        .then((response) => {
+          return response.data;
+        })
+        .then((data) => {
+          console.log(data);
+          setFoodbanks(data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.error);
+        });
+    } catch (error) {
+      console.log(error.response);
+      return error.response;
+    }
+  };
+
   return (
     <table className="table table-hover table-striped">
       <thead>
         <tr>
           <th>ID</th>
           <th>Address</th>
-          <th>Location</th>
+          {/* <th>Location</th> */}
         </tr>
       </thead>
       <tbody>
-        {json.foodBanks.map((item, i) => (
-          <tr key={i}>
-            <td>{item.id}</td>
-            <td>{item.address}</td>
-            <td>{item.location}</td>
-          </tr>
-        ))}
+        {foodbanks.length !== 0 ?
+          foodbanks.map((item, i) => (
+            <tr key={i}>
+              <td>{i+1}</td>
+              <td>{item.details}</td>
+              {/* <td>{item.location}</td> */}
+            </tr>
+          )) : <p>No records found.</p>
+        }
       </tbody>
     </table>
   )
